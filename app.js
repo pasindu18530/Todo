f3fb95faf0ca4109fc8ab4f6efb09a0845a68e16
pasindu <pasindunawagamuwa4@gmail.com>
2025-07-31 10:34:25 +0530

    interpolaration

---
+++ app.js
@@ -0,0 +1,14 @@
+function addTask() {
+  let userInput = document.getElementById("txtUserInput").value;
+  document.getElementById("todo List").innerHTML+=`<li class =<div class="card">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`
+  
+}
